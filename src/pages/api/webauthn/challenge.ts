d6bd4330b36c765aed1a4846da4bb9715a9d4348
qd-qd <qdqdqdqdqd@protonmail.com>
2022-09-16 20:02:17 +0200

    ♻️ refacto api/challenge to return a native Buffer

--- src/pages/api/webauthn/challenge.ts
+++ src/pages/api/webauthn/challenge.ts
@@ -3,7 +3,7 @@ import { generateChallenge } from "../../../utils/crypto";
 // return a random 64-characters string
 const challenge = async (_, res) => {
   const challenge = generateChallenge();
-  res.status(200).json({ challenge });
+  res.status(200).send(challenge);
 };
 
 export default challenge;
